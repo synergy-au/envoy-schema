b7380f8f584bc250b75f9672c6aef7d51565684f
Josh Vote <jjvote@gmail.com>
2024-02-23 05:58:36 +1100

    Added admin server groups

---
+++ src/envoy_schema/admin/schema/site_group.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class SiteGroupResponse(BaseModel):
+    """Response model for SiteGroup - includes basic summary information"""
+
+    site_group_id: int
+    name: str
+    changed_time: datetime
+    total_sites: int  # How many sites are linked to this group
+
+
+class SitePageResponse(BaseModel):
+    """Represents a paginated response of SiteGroup"""
+
+    total_count: int  # The total number of groups (independent of this page of results)
+    limit: int  # The maximum number of groups that could've been returned (the limit set by the query)
+    start: int  # The number of groups that have been skipped as part of this query (the start set by the query)
+    groups: list[SiteGroupResponse]  # The group models in this page
