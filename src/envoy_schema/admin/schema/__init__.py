0def4cd52d97691693c5721670a3ba6d0436bd27
Joe Crowley <joseph.crowley@synergy.net.au>
2025-06-11 09:25:38 +0800

    promoted all admin schema submodule classes to parent namespace

--- src/envoy_schema/admin/schema/__init__.py
+++ src/envoy_schema/admin/schema/__init__.py
@@ -1,1 +1,14 @@
 """Schemas represent the public facing models that are exposed via HTTP endpoints and serialised to xml/json etc"""
+
+from .aggregator import *
+from .archive import *
+from .base import *
+from .billing import *
+from .config import *
+from .doe import *
+from .log import *
+from .pricing import *
+from .site import *
+from .site_control import *
+from .site_group import *
+from .uri import *
