0babe75fa08fa830a6a61b82f4db1dfd144723a4
Joe Crowley <joseph.crowley@synergy.net.au>
2025-06-23 09:50:35 +0800

    Added incompatible pydantic version to exclude dependency. Made lfdi -> id validation xor, cannot provide both.

--- src/envoy_schema/admin/schema/certificate.py
+++ src/envoy_schema/admin/schema/certificate.py
@@ -39,12 +39,17 @@ class CertificateAssignmentRequest(pydantic.BaseModel):
     expiry: dt.datetime | None = None
 
     @pydantic.model_validator(mode="after")
-    def either_id_or_lfdi_provided(self) -> Self:
-        """Validates either id or lfdi was provided
+    def id_or_lfdi_provided_xor(self) -> Self:
+        """Validates either one of lfdi or id provided, but not both (exclusive or).
+
+        Both are identifiers of a single certificate, so ambiguity could be
+        created if both are provided. To avoid, this model rejects validation.
 
         Raises:
-            ValueError if neither id or lfdi provided
+            ValueError if neither id or lfdi provided, or if both are provided
         """
         if self.certificate_id is None and self.lfdi is None:
             raise ValueError("Either lfdi or id needs to be provided")
+        elif self.certificate_id is not None and self.lfdi is not None:
+            raise ValueError("Only one of lfdi or id can be provided, not both")
         return self
--- tests/unit/admin/test_certificate.py
+++ tests/unit/admin/test_certificate.py
@@ -6,17 +6,39 @@ import pydantic
 from envoy_schema.admin.schema.certificate import CertificateAssignmentRequest  # type: ignore[attr-defined]
 
 
-def test_assignment_request_invalid() -> None:
-    """Tests the CertificateAssignmentRequest custom validator works as expected"""
+@pytest.mark.parametrize(
+    "lfdi,certificate_id,expiry",
+    [
+        (None, None, dt.datetime(1985, 9, 30, 0, 0, 0)),
+        (None, None, None),
+    ],
+)
+def test_assignment_request_invalid_no_lfdi_or_id(
+    lfdi: str | None, certificate_id: int | None, expiry: dt.datetime | None
+) -> None:
+    """Tests the CertificateAssignmentRequest custom validator works as expected, missing id and lfdi"""
     with pytest.raises(pydantic.ValidationError, match="Either lfdi or id needs to be provided"):
-        CertificateAssignmentRequest()
+        CertificateAssignmentRequest(lfdi=lfdi, certificate_id=certificate_id, expiry=expiry)
 
 
 @pytest.mark.parametrize(
     "lfdi,certificate_id,expiry",
     [
-        ("ARST123456" * 4, 1, dt.datetime(1985, 9, 30, 0, 0, 0)),
-        ("ARST123456" * 4, 2, None),
+        ("ARST123456" * 4, 123, dt.datetime(1985, 9, 30, 0, 0, 0)),
+        ("ARST123456" * 4, 321, None),
+    ],
+)
+def test_assignment_request_invalid_both_lfdi_and_id(
+    lfdi: str | None, certificate_id: int | None, expiry: dt.datetime | None
+) -> None:
+    """Tests the CertificateAssignmentRequest custom validator works as expected, missing id and lfdi"""
+    with pytest.raises(pydantic.ValidationError, match="Only one of lfdi or id can be provided, not both"):
+        CertificateAssignmentRequest(lfdi=lfdi, certificate_id=certificate_id, expiry=expiry)
+
+
+@pytest.mark.parametrize(
+    "lfdi,certificate_id,expiry",
+    [
         ("ARST123456" * 4, None, dt.datetime(1985, 9, 30, 0, 0, 0)),
         ("ARST123456" * 4, None, None),
         (None, 121, dt.datetime(1985, 9, 30, 0, 0, 0)),
