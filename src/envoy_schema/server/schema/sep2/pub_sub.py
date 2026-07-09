7c899354b43b2aae1abb641fe4e7201779aeee08
Joe Crowley <joseph.crowley@synergy.net.au>
2026-07-09 14:01:24 +0800

    chore: formatted

--- src/envoy_schema/server/schema/sep2/pub_sub.py
+++ src/envoy_schema/server/schema/sep2/pub_sub.py
@@ -41,7 +41,6 @@ from envoy_schema.server.schema.sep2.pricing import (
     TimeTariffIntervalResponse,
 )
 from envoy_schema.server.schema.sep2.primitive_types import (
-    AbsoluteUri,
     HexBinary8,
     HexBinary32,
     HexBinary128,
