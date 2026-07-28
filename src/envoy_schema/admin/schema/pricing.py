ba87bc41eb58f4ee546bda03d0b050c2dbad3b4e
Joshua Vote <jjvote@gmail.com>
2026-07-28 16:00:43 +1000

    Added required_site_group_id to admin API (#76)

--- src/envoy_schema/admin/schema/pricing.py
+++ src/envoy_schema/admin/schema/pricing.py
@@ -25,6 +25,9 @@ class TariffRequest(BaseModel):
     price_power_of_ten_multiplier: int
     primacy: int
     fsa_id: int = 1  # The function set assignment ID that this Tariff will be grouped under
+    required_site_group_id: Optional[int] = (
+        None  # If set - only sites in this SiteGroup will "see" this Tariff. Globally visible otherwise
+    )
 
 
 class TariffResponse(TariffRequest):
